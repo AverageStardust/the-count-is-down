d651903ab6df93960f67616fe407960d6440ff9c
Taylor Crawford <123581249+dngrjeans@users.noreply.github.com>
2026-07-24 23:14:34 -0600

    rel_pos bug fix
    no longer crashes when target string is null

--- scripts/Grader.gd
+++ scripts/Grader.gd
@@ -4,10 +4,10 @@ var lev_mat = []
 var m
 var n
 
-##creates levenshtein matrix and returns levenshtein distance
-func Lev_Dis(input: String, goal: String): 
+##Creates levenshtein matrix and returns levenshtein distance
+func lev_dis(input: String, target: String) -> int: 
 	m = input.length()+1	#lev dis algorithm adds empty space in beginning
-	n = goal.length()+1
+	n = target.length()+1
 	print("size ", n, " by ", m)
 	lev_mat.resize(n*m)
 	
@@ -26,7 +26,7 @@ func Lev_Dis(input: String, goal: String):
 		line.append(lev_mat[(i+1)*m])
 		for j in range(m-1):
 			var sub_cost = 1
-			if goal[i] == input[j]: sub_cost = 0
+			if target[i] == input[j]: sub_cost = 0
 			lev_mat[(i+1)*m + j+1] = min(lev_mat[i*m + j+1] + 1,			#deletion
 										lev_mat[(i+1)*m + j] + 1,			#insertion
 										lev_mat[i*m + j] + sub_cost)	#substitution
@@ -34,12 +34,16 @@ func Lev_Dis(input: String, goal: String):
 		print(line)
 	return lev_mat[n*m-1]
 	
-##uses existing levenshtein matrix to return an array of relative locations from one string to antoher
-func Rel_Pos():
+##uses existing levenshtein matrix to return an array of locations in one string relative to another.
+##e.g.  arr[target index] = input index
+func rel_pos() -> Array[int]:
 	var j = m-1
 	var prev = lev_mat[n*m-1]+1
-	var similar = []
+	var similar : Array[int]
 	similar.resize(n-1)
+	if j == 0:
+		similar.fill(0)
+		return similar
 	for i in range(n-1,0,-1):
 		if (lev_mat[i*m + j] < prev or lev_mat[i*m + j] == lev_mat[i*m + j-1]): 
 			similar[i-1] = j-1
